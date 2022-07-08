976e756f00b064fa031c05330acc85121d1db886
İHLASMERT <ihlas1mert@gmail.com>
2022-07-08 23:52:54 +0300

    my basic react path

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id:1,
+        name:"Berite Yates",
+        age:99,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',    },
+    {
+        id:2,
+        name:"Be",
+        age:59,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',   
+    },
+    {
+        id:3,
+        name:"Berite ",
+        age:19,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',   
+    },
+    {
+        id:4,
+        name:" Yates",
+        age:39,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',   
+    }
+]
